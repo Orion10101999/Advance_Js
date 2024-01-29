b8207e786759af2a2bf00f7984667965d3bc6f90
Sandeep Kumar <orion10101999@gmail.com>
2024-01-29 10:06:49 +0530

    hof

--- 08_hof.js
+++ 08_hof.js
@@ -1,3 +1,4 @@
+/*
 let data = 10;
 Number.prototype.customFun= function(f){
     console.log(this);
@@ -7,3 +8,19 @@ Number.prototype.customFun= function(f){
 }
 
 console.log(data.customFun(x=>x*5));
+
+*/
+
+
+let data2 = [2,6,8,9,10]
+
+Array.prototype.customFun = function (x){
+    let result = []
+    for(let i = 0 ; i<this.length ; i++){
+        result.push(x(this[i]));
+    }
+    return result
+}
+
+let output = data2.customFun(x=>x*10)
+console.log(output);
